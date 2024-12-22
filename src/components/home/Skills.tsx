0429d6d64ea9b76fa335a1e1b4b7b2d1261a8410
Pierre <pierre.gaillard56@orange.fr>
2024-12-22 18:25:08 +0100

    added home skills section

---
+++ src/components/home/Skills.tsx
@@ -0,0 +1,60 @@
+import React from "react"
+
+import "./Skills.css"
+
+const Skills: React.FC = () => {
+	return (
+		<section className="background_light" id="skills">
+			<div className="container">
+				<h2>Mes compétences</h2>
+				<div className="split">
+					<div>
+						<h3>Back-end</h3>
+						<div className="split left">
+							<img src="../icons/php.svg" />
+							<h4>PHP</h4>
+						</div>
+						<div className="split left">
+							<img src="../icons/python.svg" />
+							<h4>Python</h4>
+						</div>
+					</div>
+					<div className="separator"></div>
+					<div>
+						<h3>Front-end</h3>
+						<div className="split left">
+							<img src="../icons/HTML5.svg" />
+							<h4>HTML</h4>
+						</div>
+						<div className="split left">
+							<img src="../icons/CSS.svg" />
+							<h4>CSS</h4>
+						</div>
+						<div className="split left">
+							<img src="../icons/javascript.svg" />
+							<h4>Javascript</h4>
+						</div>
+					</div>
+					<div className="separator"></div>
+					<div>
+						<h3>Outils</h3>
+						<div className="split left">
+							<img src="../icons/github-mark/github-mark-white.svg" />
+							<h4>Github</h4>
+						</div>
+						<div className="split left">
+							<img src="../icons/figma.svg" />
+							<h4>Figma</h4>
+						</div>
+						<div className="split left">
+							<img src="../icons/adobe.svg" />
+							<h4>Adobe suite</h4>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	)
+}
+
+export default Skills
