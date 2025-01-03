0f390b64cf35923eb475cead29d690a7dfc8d3da
Pierre <pierre.gaillard56@orange.fr>
2025-01-03 17:24:13 +0100

    added framer-motion dependencie updated projects page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -2,6 +2,7 @@
 import React from "react"
 import { useEffect, useState } from "react"
 import Link from "next/link"
+import { motion, AnimatePresence } from "framer-motion"
 
 import Project from "@/src/components/Project"
 
@@ -14,22 +15,22 @@ const Projects: React.FC = () => {
 		[key: string]: { active: boolean; className: string }
 	}>({})
 
-    useEffect(() => {
-        const newFilters: {
-            [key: string]: { active: boolean; className: string }
-        } = {}
-        projects.forEach((project) => {
-            project.languages.forEach((language) => {
-                if (!newFilters[language.text]) {
-                    newFilters[language.text] = {
-                        active: true,
-                        className: language.className,
-                    }
-                }
-            })
-        })
-        setFilters(newFilters)
-    }, [])
+	useEffect(() => {
+		const newFilters: {
+			[key: string]: { active: boolean; className: string }
+		} = {}
+		projects.forEach((project) => {
+			project.languages.forEach((language) => {
+				if (!newFilters[language.text]) {
+					newFilters[language.text] = {
+						active: true,
+						className: language.className,
+					}
+				}
+			})
+		})
+		setFilters(newFilters)
+	}, [])
 
 	return (
 		<div className="content projects">
@@ -63,32 +64,41 @@ const Projects: React.FC = () => {
 						)
 					})}
 				</div>
-				<div className="projectList">
-					{projects.map((project) => {
-                        if (!filters[project.languages[0].text]) return
-						if (
-							!project.languages.some(
-								(language) => filters[language.text].active
+				<AnimatePresence>
+					<div className="projectList">
+						{projects.map((project) => {
+							if (!filters[project.languages[0].text]) return
+							if (
+								!project.languages.some(
+									(language) => filters[language.text].active
+								)
+							) {
+								return
+							}
+							return (
+								<motion.div
+									className="grid-item"
+									key={project.id}
+									layout
+									initial={{ opacity: 0 }}
+									animate={{ opacity: 1 }}
+									exit={{ opacity: 0 }}
+									transition={{ duration: 0.5 }}
+								>
+									<Link href={`/projects/${project.id}`}>
+										<Project
+											id={project.id}
+											title={project.title}
+											img={project.img}
+											description={project.description}
+											Languages={project.languages}
+										/>
+									</Link>
+								</motion.div>
 							)
-						) {
-							return
-						}
-						return (
-							<Link
-								href={`/projects/${project.id}`}
-								key={project.id}
-							>
-								<Project
-									id={project.id}
-									title={project.title}
-									img={project.img}
-									description={project.description}
-									Languages={project.languages}
-								/>
-							</Link>
-						)
-					})}
-				</div>
+						})}
+					</div>
+				</AnimatePresence>
 			</section>
 		</div>
 	)
